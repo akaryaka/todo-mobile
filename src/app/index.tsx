cc45d080701e3a8f4845dce70c289a19ef7a44ef
andrew <akariaka@yandex.ru>
2026-09-02 23:22:51 +0600

    output tasks

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,37 +1,50 @@
-import { useState } from "react";
-import { StyleSheet, Text, TextInput, View } from "react-native";
+import { FlatList, StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
   const listTasks = [
     { id: 1, title: "task1", desc: "desc" },
     { id: 2, title: "task2", desc: "desc2" },
   ];
-  const [todos, setTodos] = useState(listTasks);
-  function addTodo() {
-    const newTodo = {
-      id: todos.length + 1,
-      title: `title ${todos.length + 1}`,
-      desc: `desc ${todos.length + 1}`,
-    };
-    let arr = todos;
-    arr.push(newTodo);
-    console.log(todos);
+  // const [todos, setTodos] = useState(listTasks);
 
-    setTodos(arr);
-  }
+  // function addTodo() {
+  //   const newTodo = {
+  //     id: todos.length + 1,
+  //     title: `title ${todos.length + 1}`,
+  //     desc: `desc ${todos.length + 1}`,
+  //   };
+  //   let arr = todos;
+  //   arr.push(newTodo);
+  // }
+
+  type Props = {
+    title: string;
+  };
+
+  const Item = ({ title }: Props) => {
+    return (
+      <>
+        <View>
+          <Text>{title}</Text>
+        </View>
+      </>
+    );
+  };
 
   return (
     <>
-      <View>
-        <Text>Todo</Text>
-        <TextInput defaultValue="название дела" />
-        <button>add</button>
-      </View>
-      <ul>
-        {todos.map((item, index) => (
-          <li key={index}>{item.title}</li>
-        ))}
-      </ul>
+      <SafeAreaProvider>
+        <SafeAreaView>
+          <FlatList
+            data={listTasks}
+            renderItem={({ item }) => <Item title={item.title} />}
+            keyExtractor={(item) => item.id}
+          >
+            <Text>нет задач</Text>
+          </FlatList>
+        </SafeAreaView>
+      </SafeAreaProvider>
     </>
   );
 }
